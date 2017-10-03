e0a46d810a33e4cd0376b9950904d8148a566c84
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 14:19:58 -0700

    Check number in range

--- game.py
+++ game.py
@@ -1,23 +1,28 @@
 """A number-guessing game."""
 
 # Put your code here
-import random 
+import random
+
 name = raw_input("Hi, what's your name?\n")
+
 print "{}, I'm thinking of a number between 1 and 100 (inclusive).\n".format(name)
 print "Try to guess my number.\n"
-correct_num = random.randint(1,100)
-guess_count = 0 
+
+correct_num = random.randint(1, 100)
+guess_count = 0
 guess = None
 
 print correct_num
 
 while guess != correct_num:
     guess = int(raw_input("What is your guess?\n"))
-    guess_count += 1
-    if guess < correct_num: 
-        print "Too low! Guess again. Total guesses so far: {}".format(guess_count)
-    elif guess > correct_num: 
-        print "Too high! Guess again."
-    elif guess == correct_num:
-        print "Congrats, you guessed correctly after {} guesses!".format(guess_count)
-   
+    if guess < 1 or guess > 100:
+        print "Out of range, guess again"
+    else:
+        guess_count += 1
+        if guess < correct_num:
+            print "Too low! Guess again. Total guesses so far: {}".format(guess_count)
+        elif guess > correct_num:
+            print "Too high! Guess again."
+        elif guess == correct_num:
+            print "Congrats, you guessed correctly after {} guesses!".format(guess_count)
