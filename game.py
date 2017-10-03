3c5d92694b2862d184bb9a049a8618d22e627afe
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 14:06:46 -0700

    Play the game

--- game.py
+++ game.py
@@ -5,19 +5,19 @@ import random
 name = raw_input("Hi, what's your name?\n")
 print "{}, I'm thinking of a number between 1 and 100 (inclusive).\n".format(name)
 print "Try to guess my number.\n"
-# correct_num = random.randint(1,100)
-# guess_count = 0 
-# guess = None
+correct_num = random.randint(1,100)
+guess_count = 0 
+guess = None
 
-# print correct_num
+print correct_num
 
-# while guess != correct_num:
-#     guess = int(raw_input("What is your guess?"))
-#     guess_count += 1
-#     if guess < correct_num: 
-#         print "Too low! Guess again. Total guesses so far: {}".format(guess_count)
-#     elif guess > correct_num: 
-#         print "Too high! Guess again."
-#     elif guess == correct_num:
-#         print "Congrats, you guessed correctly after {} guesses!".format(guess_count)
+while guess != correct_num:
+    guess = int(raw_input("What is your guess?"))
+    guess_count += 1
+    if guess < correct_num: 
+        print "Too low! Guess again. Total guesses so far: {}".format(guess_count)
+    elif guess > correct_num: 
+        print "Too high! Guess again."
+    elif guess == correct_num:
+        print "Congrats, you guessed correctly after {} guesses!".format(guess_count)
    
