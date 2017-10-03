f348831cbf45f9d913640e8118053fd6ee7644d4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 14:04:06 -0700

    Ask for number

--- game.py
+++ game.py
@@ -3,8 +3,8 @@
 # Put your code here
 import random 
 name = raw_input("Hi, what's your name?\n")
-# print "{}, I'm thinking of a number between 1 and 100 (inclusive).\n".format(name)
-# print "Try to guess my number.\n"
+print "{}, I'm thinking of a number between 1 and 100 (inclusive).\n".format(name)
+print "Try to guess my number.\n"
 # correct_num = random.randint(1,100)
 # guess_count = 0 
 # guess = None
